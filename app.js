d001f88bd293a8bf08130c679e65b91b9d4591c1
Edd Coria <jegcoria@gmail.com>
2023-10-06 15:08:59 +0200

    API1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const morgan = require("morgan");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const PORT = 5000;
+const PORT = 3001;
 
 server.use(middlewares);
 server.use(morgan("dev"));
